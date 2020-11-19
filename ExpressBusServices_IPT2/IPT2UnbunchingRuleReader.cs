51a5e4bb25e94a1ac672cfa39c88dc4915e607d1
Vectorial1024 <vincentwwy168@gmail.com>
2020-11-19 22:48:37 +0800

    Basic framework to correct the insta-depart behavior

---
+++ ExpressBusServices_IPT2/IPT2UnbunchingRuleReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImprovedPublicTransport2;
+
+namespace ExpressBusServices_IPT2
+{
+    public class IPT2UnbunchingRuleReader
+    {
+        public enum InterpretationMode
+        {
+            FIRST_PRINCIPLES,
+            RESPECT_IPT2,
+            INVERT_IPT2,
+        }
+
+        public static InterpretationMode CurrentRuleInterpretation { get; set; }
+
+        public static bool ReadAndInterpretCanInstantlyDepartNow(ushort vehicleID, ref Vehicle vehicleData)
+        {
+            ushort currentStop = CachedVehicleData.m_cachedVehicleData[vehicleID].CurrentStop;
+            if (currentStop == 0)
+            {
+                // we have no idea what is going on. allow them to proceed.
+                return true;
+            }
+            bool stopIsUsingUnbunching = CachedNodeData.m_cachedNodeData[currentStop].Unbunching;
+            bool lineIsUsingUnbunching = CachedTransportLineData.GetUnbunchingState(vehicleData.m_transportLine);
+            switch (CurrentRuleInterpretation)
+            {
+                case InterpretationMode.RESPECT_IPT2:
+                    // allow insta depart if 
+                    // unbunching for line is disabled, or
+                    // unbunching for line is active and unbunching at this stop is inactive
+                    return !lineIsUsingUnbunching || (!stopIsUsingUnbunching);
+                case InterpretationMode.INVERT_IPT2:
+                    // allow insta depart if
+                    // unbunching for line is disabled, or
+                    // unbunching for line is active and unbinching at this stop is allowed
+                    // this is mainly for convenience where players need not go to every stop
+                    // and flick the unbunching toggle to make it NOT unbunch at all the intermediate stops
+                    return !lineIsUsingUnbunching || stopIsUsingUnbunching;
+                default:
+                    // dont block if somehow cannot determine if should insta depart
+                    return true;
+            }
+        }
+    }
+}
---
+++ ExpressBusServices_IPT2/Patch_DepartureChecker_CheckEligibleForInstaDepart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using ImprovedPublicTransport2;
+
+namespace ExpressBusServices_IPT2
+{
+    [HarmonyPatch(typeof(BusAI))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_DepartureChecker_CheckEligibleForInstaDepart
+    {
+        [HarmonyPostfix]
+        public static void PostFix(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            if (IPT2UnbunchingRuleReader.CurrentRuleInterpretation == IPT2UnbunchingRuleReader.InterpretationMode.FIRST_PRINCIPLES)
+            {
+                // this is the original default.
+                return;
+            }
+            // Determine if we are allowed to depart immediately by reading the IPT2 settings.
+            if (IPT2UnbunchingRuleReader.ReadAndInterpretCanInstantlyDepartNow(vehicleID, ref vehicleData))
+            {
+                __result = true;
+            }
+            return;
+        }
+    }
+}
