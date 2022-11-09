3a14f6ce94dae9862b19eb4035ed3d80d15ee876
Vincent Wong <vincentwwy168@gmail.com>
2022-11-09 21:31:39 +0800

    Unexpected update: connect IPT2 into the main mod
    I somehow missed this... some files are as early as July 2022

---
+++ ExpressBusServices_IPT2/Patch_Detour_TrolleybusAI_DepartCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressBusServices;
+using HarmonyLib;
+using ImprovedPublicTransport2;
+using ImprovedPublicTransport2.Detour.Vehicles;
+
+namespace ExpressBusServices_IPT2
+{
+    [HarmonyPatch(typeof(BusAIDetour))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_Detour_TrolleybusAI_DepartCheck
+    {
+        [HarmonyPostfix]
+        public static void PostFix(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            // we just call the code on our side one more time in case our side fails
+            BusPickDropLookupTable.DetermineIfBusShouldDepart(ref __result, vehicleID, ref vehicleData);
+        }
+    }
+}
